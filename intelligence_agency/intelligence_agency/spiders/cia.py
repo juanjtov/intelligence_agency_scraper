39997399c5cdef40d8128dc4336a7ef5fada9166
Juan Tovar <jujotomo@gmail.com>
2020-11-15 06:13:29 -0800

    Methods parse and parse_link have been created, ready to the frontend

--- intelligence_agency/intelligence_agency/spiders/cia.py
+++ intelligence_agency/intelligence_agency/spiders/cia.py
@@ -1,9 +1,11 @@
 import scrapy
 
 # links = xpath('//a[starts-with(@href, "collection") and (parent::h3|parent::h2)]/@href').getall()
+# title = xpath('//h1[@class="documentFirstHeading"]/text()').get()
+# paragraph =  xpath('//div[@class="field-item even"]//p[not(@class)]/text()').getall()
 
 
-class SpiderCIA(scrapy.spider):
+class SpiderCIA(scrapy.Spider):
     name = 'cia'
     start_urls = [
         'https://www.cia.gov/library/readingroom/historical-collections']
@@ -18,7 +20,23 @@ class SpiderCIA(scrapy.spider):
         links_desclassified = response.xpath(
             '//a[starts-with(@href, "collection") and (parent::h3|parent::h2)]/@href').getall()
         for link in links_desclassified:
-            yield response.follow(link, callback=self.parse_link)
+            yield response.follow(link, callback=self.parse_link, cb_kwargs={'url': response.urljoin(link)})
+            # urljoin allow us to join the secondary link got by the scraper with the main link
 
-    def parse_link(self, response):
-        pass
+    def parse_link(self, response, **kwargs):
+        link = kwargs['url']
+        title = response.xpath(
+            '//h1[@class="documentFirstHeading"]/text()').get()
+        paragraph = response.xpath(
+            '//div[@class="field-item even"]//p[not(@class)]/text()').getall()  # you can send the list of paragraphs or just the first
+
+        if len(paragraph[0]) > 30:
+            paragraph = paragraph[0]
+        else:
+            paragraph = paragraph[1]
+
+        yield {
+            'url': link,
+            'title': title,
+            'body': paragraph
+        }
